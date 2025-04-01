2394108f1bf61a75b5413832dfd16c8b07dfc87d
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-04-01 17:09:32 -0300

    init fundamentos-reactjs

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from "./Post"
+import './styles.css'
+
+function App() {
+
+  return (
+    <div>
+      <Post 
+      author="Jotinha Da Play"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+      />
+      <Post 
+      author="Dudinha Xuxu"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+      />
+    </div>
+  )
+}
+
+
+export default App
