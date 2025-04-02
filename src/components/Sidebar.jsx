d506d7c6ff2e7bc904338fe5c9ebd0a9ad63240d
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-04-01 23:35:42 -0300

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,29 @@
+import { PencilLine } from "@phosphor-icons/react";
+
+import styles from './Sidebar.module.css'
+
 export function Sidebar() {
     return (
-        <aside>
-            Sidebar
+        <aside className={styles.sidebar}>
+            <img 
+            className={styles.cover} 
+            src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://xesque.rocketseat.dev/users/avatar/profile-35fbe46d-88c8-4083-ade4-2fcde39eccff-1709175086396.jpg" />
+                <strong>João Marcelo Vilas Boas</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={18} />
+                    Editar seu perfil
+                </a>
+            </footer>
+            
+            
         </aside>
     )
 }
