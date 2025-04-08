9457254a9a52783f83b3b08ebdc481a2056c8593
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-04-08 00:21:02 -0300

    adiciona informa;'oes no componente post de forma dinamica

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import { Post } from "./components/Post"
 import './global.css'
 import styles from'./App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://xesque.rocketseat.dev/users/avatar/profile-35fbe46d-88c8-4083-ade4-2fcde39eccff-1709175086396.jpg',
+      name: 'João Marcelo Vilas Boas',
+      role: 'Desenvolvedor Web'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz com React 🚀' }, 
+      { type: 'link', content: '#React' },
+    ],
+    publishedAt: new Date('2025-04-04 10:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Bom dia pessoal!' },
+      { type: 'paragraph', content: 'Vamos aprender mais sobre react?' }, 
+    ],
+    publishedAt: new Date('2025-04-02 12:00:00'),
+  }
+]
+
 function App() {
 
   return (
@@ -13,14 +44,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="João Marcelo Vilas Boas"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-          />
-          <Post 
-            author="João Marcelo Vilas Boas"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-          />
+          {posts.map( post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                key={post.id}
+              />
+            )
+          })}
+          
         </main>
       </div>
 
