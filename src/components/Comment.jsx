1f856b13d7e41e508e769dd875d6b8b4316949c5
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-04-10 23:37:50 -0300

    adiciona conteúdo do comentario de acordo com input

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://xesque.rocketseat.dev/users/avatar/profile-35fbe46d-88c8-4083-ade4-2fcde39eccff-1709175086396.jpg" />
@@ -20,7 +20,7 @@ export function Comment() {
                         </button>
                     </header>
 
-                    <p>Muito bom Dev, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>    
